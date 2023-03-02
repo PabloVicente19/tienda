9df4a5f663d25d5bda40e201c4da842af0714d9f
Pablo Vicente <pablovicente19@hotmail.com>
2023-03-02 13:12:05 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+const $cardContainer = document.querySelector('.card-product-container');
+
+const createCard = (product) => {
+  const {id, imagen, marca, modelo, precio} = product;
+  return `
+  <div class="card-product">
+    <div class="card-product-img-container">
+      <img class="card-product-img" src=${imagen} alt=${modelo} ${marca}>
+    </div>
+    <h2 class="card-product-title">${marca} ${modelo}</h2>
+    <span class="card-product-price">Precio: $${precio}</span>
+    <button class="card-product-btn" data-id=${id}>Agregar Al Carrito</button>
+  </div>
+  `
+}
+
+const renderCard = (product) =>{
+  return $cardContainer.innerHTML = product.map(producto => createCard(producto) );
+}
+
+const init = () => {
+  renderCard(products)
+}
+init()
---
+++ src/js/products.js
@@ -0,0 +1,51 @@
+const products = [
+  {
+    id: 1,
+    marca: "adidas",
+    modelo: "forum",
+    precio: 14054,
+    imagen: "./src/imgs/shoes/adidas-forum.jpg"
+  },
+  {
+    id: 2,
+    marca: "adidas",
+    modelo: "street",
+    precio: 13000,
+    imagen: "./src/imgs/shoes/adidas-streetcheck.jpg"
+  },
+  {
+    id: 3,
+    marca: "fila",
+    modelo: "jogger",
+    precio: 14054,
+    imagen: "./src/imgs/shoes/fila-jogger-sport.jpg"
+  },
+  {
+    id: 4,
+    marca: "nike",
+    modelo: "air max",
+    precio: 14054,
+    imagen: "./src/imgs/shoes/nike-air-max-90.jpg"
+  },
+  {
+    id: 5,
+    marca: "nike",
+    modelo: "shifters",
+    precio: 14054,
+    imagen: "./src/imgs/shoes/nike-downshifter-11.jpg"
+  },
+  {
+    id: 6,
+    marca: "nike",
+    modelo: "retro se ",
+    precio: 14054,
+    imagen: "./src/imgs/shoes/nike-retro-se.jpg"
+  },
+  {
+    id: 7,
+    marca: "nike",
+    modelo: "revolution",
+    precio: 14054,
+    imagen: "./src/imgs/shoes/nike-revolution-5.jpg"
+  },
+]
